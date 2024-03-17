2d235641db6fd7c626cfc7f6c3afb860878a8538
Tautminas Cibulskis <tautminas@yahoo.com>
2024-03-17 13:38:18 +0200

    Add physics to level and player

--- player.gd
+++ player.gd
@@ -1,9 +1,18 @@
-extends Node3D
+extends RigidBody3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		apply_central_force(Vector3.UP * delta * 1000.0)
+	
 	if Input.is_action_pressed("ui_left"):
-		position.x -= delta * 100
+		apply_central_force(Vector3.LEFT * delta * 1000.0)
 	
 	if Input.is_action_pressed("ui_right"):
-		position.x += delta * 100
+		apply_central_force(Vector3.RIGHT * delta * 1000.0)
+
+	if Input.is_action_pressed("ui_up"):
+		apply_central_force(Vector3.FORWARD * delta * 1000.0)
+		
+	if Input.is_action_pressed("ui_down"):
+		apply_central_force(Vector3.BACK * delta * 1000.0)		
