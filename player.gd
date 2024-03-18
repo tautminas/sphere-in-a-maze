773abc62cb2964cd585f50944b333b53cd88ec89
Tautminas Cibulskis <tautminas@yahoo.com>
2024-03-18 18:00:52 +0200

    Add export annotation

--- player.gd
+++ player.gd
@@ -1,21 +1,28 @@
 extends RigidBody3D
 
+## How much force to apply when moving on the floor.
+@export_range(750.0, 3000.0) var force_magnitude : float = 1000.0
+
+## How much force to apply when jumping.
+@export var thrust: float = 1500.0
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("jump"):
-		apply_central_force(Vector3.UP * delta * 1000.0)
+		apply_central_force(Vector3.UP * delta * thrust)
 	
 	if Input.is_action_pressed("left"):
-		apply_central_force(Vector3.LEFT * delta * 1000.0)
+		apply_central_force(Vector3.LEFT * delta * force_magnitude)
 	
 	if Input.is_action_pressed("right"):
-		apply_central_force(Vector3.RIGHT * delta * 1000.0)
+		apply_central_force(Vector3.RIGHT * delta * force_magnitude)
 
 	if Input.is_action_pressed("up"):
-		apply_central_force(Vector3.FORWARD * delta * 1000.0)
+		apply_central_force(Vector3.FORWARD * delta * force_magnitude)
 		
 	if Input.is_action_pressed("down"):
-		apply_central_force(Vector3.BACK * delta * 1000.0)
+		apply_central_force(Vector3.BACK * delta * force_magnitude)
 
 
 func _on_body_entered(body: Node) -> void:
