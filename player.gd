fee16a9d1cf42107d9509124fc1f8a398f36f4da
Tautminas Cibulskis <tautminas@yahoo.com>
2024-03-20 00:05:08 +0200

    Add CSG walls for the maze level 1

--- player.gd
+++ player.gd
@@ -1,10 +1,10 @@
 extends RigidBody3D
 
 ## How much force to apply when moving on the floor.
-@export_range(750.0, 3000.0) var force_magnitude : float = 1000.0
+@export_range(750.0, 3000.0) var force_magnitude : float = 2750.0
 
 ## How much force to apply when jumping.
-@export var thrust: float = 1500.0
+@export var thrust: float = 2500.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
